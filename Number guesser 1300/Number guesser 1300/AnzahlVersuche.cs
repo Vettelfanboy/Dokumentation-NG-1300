78ec76e8cca04d87fdc4d354030ff0c7418b8de5
Vettelfanboy <110892495+Vettelfanboy@users.noreply.github.com>
2023-09-08 10:47:40 +0200

    Add files via upload

---
+++ Number guesser 1300/Number guesser 1300/AnzahlVersuche.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Number_guesser_1300
+{
+    internal class AnzahlVersuche
+    {
+        
+            
+       public int gebrauchteVersuche;
+
+
+        // selbstständig gemacht
+        public AnzahlVersuche()
+        {
+           
+            this.gebrauchteVersuche = 0;
+        }
+    
+    
+    }  
+
+}
+
